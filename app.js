3874024ef344cf697e82dfe8fd04e72434d3e2b1
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2022-12-17 02:02:31 +0530

    rapido commit

--- app.js
+++
@@ -1,7 +0,0 @@
-addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-      document.getElementById("invisible1").style.opacity = 1;
-  } else if (window.scrollY < 200) {
-      document.getElementById("invisible1").style.opacity = 0;
-  }
-});
